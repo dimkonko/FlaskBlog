b92b7f13df263abf9ecbf1a95eabe45d2fce52f9
Dima Karacheban <d.karacheban@gmail.com>
2014-08-26 08:25:19 +0000

    Added project to a server

--- main_page/view.py
+++ main_page/view.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template
 main_page = Blueprint("main_page", __name__,
                       template_folder="templates")
 
-@main_page.route("/home")
+@main_page.route("/")
 def lol():
     return render_template("blog.html")
 
--- run.py
+++ run.py
@@ -1,14 +1,8 @@
 from flask import Flask
 from main_page.view import main_page
-from chat.view import chat
 
 app = Flask(__name__)
 app.register_blueprint(main_page)
-app.register_blueprint(chat)
-
-@app.route("/")
-def main():
-	return "Hello!"
 
 if __name__ == "__main__":
     app.run(debug=True)
