2c81e193e624a1d5715949c0f2400ed918e3bd71
Dima Karacheban <d.karacheban@gmail.com>
2014-09-08 19:09:36 +0300

    Fixed encoding, etc.

--- database/db.sql
+++ database/db.sql
@@ -6,10 +6,12 @@ CREATE TABLE post (
     date DATE NOT NULL,
     PRIMARY KEY(id)
 );
+alter table post convert to character set utf8 COLLATE utf8_unicode_ci;
 
 CREATE TABLE users (
 	id INT NOT NULL AUTO_INCREMENT,
 	f_name VARCHAR(100) NOT NULL,
 	l_name VARCHAR(100) NOT NULL,
 	PRIMARY KEY(id)
-)
+);
+alter table post convert to character set utf8 COLLATE utf8_unicode_ci;
--- main_page/model.py
+++ main_page/model.py
@@ -5,6 +5,8 @@ from custom_modules.ConfigLoader import ConfigLoader
 
 class MainModel(object):
     def __init__(self):
+        self.encoding = "utf-8"
+        
         config = ConfigLoader(sys.path[0] + "/conf/db_conf.ini").load()
         self.host = config["host"]
         self.user = config["user"]
@@ -16,28 +18,34 @@ class MainModel(object):
                            passwd=self.passwd, db=self.db)
 
     def add_post(self, post_form):
-        for key, value in post_form.iteritems():
-            value = value.encode('utf8')
-        print post_form
-
         sql = "INSERT INTO post(title, descr, link, date) " +\
-              "VALUES('{0}', '{1}', '{2}', CURDATE());".format(post_form["title"],
-                post_form["descr"], post_form["link"])
-        self._execute_sql(sql)
+              "VALUES('{0}', '{1}', '{2}', CURDATE());".format(
+                    post_form["title"].encode(self.encoding),
+                    post_form["descr"].encode(self.encoding),
+                    post_form["link"].encode(self.encoding)
+                    )
+        db = self.conn()
+        cursor = db.cursor()
+        cursor.execute(sql)
+        db.commit()
+        db.close()
         return True
 
     def get_all(self):
+        sql = "SELECT * FROM post"
+
         db = self.conn()
         cursor = db.cursor()
-        sql = "SELECT * FROM post"
         cursor.execute(sql)
         data_list = cursor.fetchall()
         db.close()
-        return data_list
 
-    def _execute_sql(self, sql_string):
-        db = self.conn()
-        cursor = db.cursor()
-        cursor.execute(sql_string)
-        db.commit()
-        db.close()
+        posts = list()
+        for post in data_list:
+            e_post = list()
+            for item in post:
+                if type(item) is str:
+                    item = item.decode(self.encoding)
+                e_post.append(item)
+            posts.append(e_post)
+        return posts
